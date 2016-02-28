2bb5f71fa8fdb5a5c8de2eb3993666bb4d85b2c4
Jon Ball <jrball3@verizon.net>
2016-02-28 14:16:33 -0500

    Part 1

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -4,8 +4,10 @@ Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
     # each returned element will be a hash whose key is the table header.
     # you should arrange to add that movie to the database here.
+    this_movie = Movie.new(title: movie[:title], rating: movie[:rating], 
+                                            release_date: movie[:release_date])
+    this_movie.save
   end
-  fail "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
